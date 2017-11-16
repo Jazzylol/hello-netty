1a921c7bfd837acdabbb7b7ae0f44fb10a18f926
Jazz <506496088@qq.com>
2017-11-16 23:58:13 +0800

    [add] channel demo

---
+++ src/main/java/io/netty/learn/java/nio/selector/SelectorDemo_1.java
@@ -0,0 +1,26 @@
+package io.netty.learn.java.nio.selector;
+
+
+import java.nio.channels.Selector;
+
+public class SelectorDemo_1 {
+
+
+    public static void main(String[] args) throws Exception {
+
+
+        test1();
+
+    }
+
+    private static void test1() throws Exception {
+
+        Selector open = Selector.open();
+
+
+
+
+    }
+
+
+}
